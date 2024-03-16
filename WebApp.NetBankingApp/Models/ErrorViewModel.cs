7bb24d9cd94197afe85ba7182c6fdacebcd96d3e
AKAPharis <132085671+AKAPharis@users.noreply.github.com>
2024-03-16 02:56:58 -0400

    Add project files.

---
+++ WebApp.NetBankingApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApp.NetBankingApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
