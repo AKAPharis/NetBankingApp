906d2e057979fc543a57401378082f5e9bff7a5a
Dariana Cabreja <darianacabreja@gmail.com>
2024-03-19 00:57:20 -0400

    Login ready

--- WebApp.NetBankingApp/Program.cs
+++ WebApp.NetBankingApp/Program.cs
@@ -36,7 +36,7 @@ namespace WebApp.NetBankingApp
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=User}/{action=Index}/{id?}");
 
             app.Run();
         }
