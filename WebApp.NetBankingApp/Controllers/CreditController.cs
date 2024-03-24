17a937b09e3a2a8375da9652cf52124b48bf4539
Waniel Torres <alexk7320@gmail.com>
2024-03-24 02:20:12 -0500

    Added Controllers and Views (Structure)

---
+++ WebApp.NetBankingApp/Controllers/CreditController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.NetBankingApp.Controllers
+{
+    [Authorize]
+    public class CreditController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApp.NetBankingApp/Controllers/PayController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.NetBankingApp.Controllers
+{
+    [Authorize]
+    public class PayController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
