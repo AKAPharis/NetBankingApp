546fab75975360775cc1dd25ea592197f083931d
AKAPharis <132085671+AKAPharis@users.noreply.github.com>
2024-03-25 02:56:23 -0400

    Adding the transaction express and beneficiary

--- NetBankingApp.Core.Application/ViewModels/Transaction/TransactionBeneficiaryViewModel.cs
+++ NetBankingApp.Core.Application/ViewModels/Transaction/TransactionBeneficiaryViewModel.cs
@@ -1,11 +1,20 @@
-﻿namespace NetBankingApp.Core.Application.ViewModels.Transaction
+﻿using NetBankingApp.Core.Application.ViewModels.Beneficiary;
+using NetBankingApp.Core.Application.ViewModels.SavingAccount;
+
+namespace NetBankingApp.Core.Application.ViewModels.Transaction
 {
     public class TransactionBeneficiaryViewModel
     {
-        public string IdUser { get; set; }
+        public string? IdUser { get; set; }
         public string GuidAccountOrigin { get; set; }
 
         public string IdBeneficiary { get; set; }
         public double Amount { get; set; }
+        public List<SavingAccountViewModel>? SavingAccounts { get; set; }
+        public List<BeneficiaryViewModel>? Beneficiaries { get; set; }
+
+
+        public string? Error { get; set; }
+        public bool HasError { get; set; }
     }
 }
